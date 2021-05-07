ea649678627cb75e81610618b53a5e8568249cd1
Axelvel <axelvel@live.fr>
2021-05-07 18:16:40 +0200

    :sparkles: Implemented CBC

--- main.py
+++ main.py
@@ -3,11 +3,62 @@
 import hashlib
 import binascii
 
+def encrypt_cbc(m, iv , e, n): #Cypher Block Chaining
+
+
+    #Division par blocs
+    block_list = [char for char in m]
+
+    #Conversion char en int
+    for i in range(len(block_list)):
+        block_list[i] = home_string_to_int(block_list[i])
+
+    initialization_vector = iv
+
+    #Initial encryption
+    print(block_list)
+    block_list[0] = initialization_vector ^ block_list[0]
+
+    #encryption first block
+
+    block_list[0] = home_mod_expnoent(block_list[0], e, n)
+
+
+    for i in range(1, len(block_list)):
+        print(i)
+        print(block_list)
+        block_list[i] = block_list[i-1] ^ block_list[i]
+        block_list[i] = home_mod_expnoent(block_list[i], e, n)
+
+    return block_list
+
+
+def decrypt_cbc(block_list, iv, d, p, q):
+
+    message_list = [None] * len(block_list)
+
+    #message_list[0] = home_mod_expnoent(block_list[0], d, n)
+    c = block_list[0]
+    message_list[0] = mod_exp_crt(p, q, d, c)
+    message_list[0] = message_list[0] ^ iv
+
+    for i in range(1, len(block_list)):
+        #message_list[i] = home_mod_expnoent(block_list[i], d, n)  #TODO: Implement CRT for message decryption
+        c = block_list[i]
+        message_list[i] = mod_exp_crt(p, q, d, c)
+        message_list[i] = message_list[i] ^ block_list[i-1]
+
+    #Conversion int en cha
+    for i in range(len(block_list)):
+        message_list[i] = home_int_to_string(message_list[i])
+    
+    return message_list
+
 def mod_exp_crt(p, q, d, c):  #Modular exponentiation using CRT Algorithm (decrypting encrypyed message)
 
     if (q > p): p,q = q,p #q < p
     n = p*q
-    q_inv = home_ext_euclide(p, q) ##inverse modulaire de q mod p
+    q_inv = home_ext_euclide(p, q) #Inverse modulaire de q mod p
     dq = d % (q-1)
     dp = d % (p-1)
 
@@ -66,7 +117,7 @@ def home_int_to_string(x): # pour transformer un int en string
 
 
 
-
+"""
 def mot10char(): #entrer le secret
     secret=input("donner un secret de 10 caractères au maximum : ")
     while (len(secret)>50):
@@ -80,28 +131,27 @@ def mot50char(): #entrer le secret
     while (len(secret)>51):
         secret=input("c'est beaucoup trop long, 50 caractères S.V.P : ")
     return(secret)
+"""
     
 
-#voici les éléments de la clé d'Alice
-#x1a=2010942103422233250095259520183 #p
-#x2a=3503815992030544427564583819137 #q
+#Alice's keys
 
 x1a=46729851468429981450596683934659799417296757155707833262801920174014214271791880250080149828203777515317 #p1 for sha256 (104 digits keys)
 x2a=78576609692768067820821754278289758196321966686126130368838717805087052908022138028011573028777628837617 #q1 for sha256
 na=x1a*x2a  #n
 phia=((x1a-1)*(x2a-1))//home_pgcd(x1a-1,x2a-1)
-ea=17 #exposant public
-da=home_ext_euclide(phia,ea) #exposant privé
-#voici les éléments de la clé de bob
-#x1b=9434659759111223227678316435911 #p
-#x2b=8842546075387759637728590482297 #q
+ea=17 #Public key
+da=home_ext_euclide(phia,ea) #Private key
+
+#Bob's key
+
 x1b=44974862217905051299604905779732073696318755181134355304715504703900165445769065766434794585211944116773 #p2 for sha256
 x2b=27036107090286352347612559531240494672462139123470458244545933143045120802035142478857262597331090531877 #q2 for sha256
 
 nb=x1b*x2b # n
 phib=((x1b-1)*(x2b-1))//home_pgcd(x1b-1,x2b-1)
-eb=23 # exposants public
-db=home_ext_euclide(phib,eb) #exposant privé
+eb=23 #Public key
+db=home_ext_euclide(phib,eb) #Private key
 
 
 
@@ -120,7 +170,8 @@ print("il est temps de lui envoyer votre secret ")
 print("*******************************************************************")
 x=input("appuyer sur entrer")
 #secret=mot10char() #TODO: Augmenter taille message
-secret=mot50char()
+#secret=mot50char()
+secret=input("donner un secret de 10 caractères au maximum : ")
 print("*******************************************************************")
 print("voici la version en nombre décimal de ",secret," : ")
 num_sec=home_string_to_int(secret)
@@ -168,3 +219,22 @@ if (Ahachis3 - designe == 0):
     print("Alice : Bob m'a envoyé : ",dechif)
 else:
     print("oups")
+
+print("CBC : \n")
+iv = 2500000293823823820 #TODO : Generate other IV
+
+def convert(s):
+  
+    # initialization of string to ""
+    str1 = ""
+  
+    # using join function join the list s by 
+    # separating words by str1
+    return(str1.join(s))
+
+lala = encrypt_cbc(secret, iv, ea, na)
+print(lala)
+print("\n")
+lolo = decrypt_cbc(lala, iv, da, x1a, x2a)
+
+print (convert(lolo))
